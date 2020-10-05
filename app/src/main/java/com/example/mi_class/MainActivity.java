8e3e9f0188ccb85c44559021b836a5848381b41b
saidi <1466913327@qq.com>
2020-10-05 15:41:51 +0800

    导航栏添加按钮

--- app/src/main/java/com/example/mi_class/MainActivity.java
+++ app/src/main/java/com/example/mi_class/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.mi_class;
 
+import android.app.ActionBar;
+import android.content.ClipData;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +23,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+
+    private int fragmentIndex = 0;
 
     private final int[][] icons = {
             {R.drawable.ic_courses,R.drawable.ic_courses_checked},
@@ -43,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         // 状态栏文字自适应
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         setContentView(R.layout.activity_main);
-
+        setTitle("全部课程");
         viewPager = findViewById(R.id.view_pager);
         initToolbar();
     }
@@ -69,20 +77,50 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 //        getActionBar().setTitle();
+        fragmentIndex = position;
     }
 
     @Override
     public void onPageSelected(int position) {
+        fragmentIndex = position;
         int index = 0;
         int len = fragments.size();
         while (index < len) {
             fragments.get(index).onHiddenChanged(index != position);
             index++;
         }
+        switch (position){
+            case 0 : setTitle("全部课程");break;
+            case 1 : setTitle("信息");break;
+            case 2 : setTitle("我的");break;
+         }
     }
 
     @Override
     public void onPageScrollStateChanged(int state) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_add_btn, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == R.id.action_cart) {//监听菜单按钮
+            Toast.makeText(this, "add selected!", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu){
+        MenuItem item = menu.findItem(R.id.action_cart);
+        item.setVisible(fragmentIndex == 0);
+
+        return super.onPrepareOptionsMenu(menu);
     }
 }
