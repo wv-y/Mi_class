10ded201df18ef9f4c64188aa03cc0dff29682a3
wv-y <2995398018@qq.com>
2020-10-12 09:31:11 +0800

    课程相关界面修改

--- app/src/main/java/com/example/mi_class/activity/CourseDetailsActivity.java
+++ app/src/main/java/com/example/mi_class/activity/CourseDetailsActivity.java
@@ -14,10 +14,10 @@ import android.widget.ImageView;
 import com.example.mi_class.R;
 import com.example.mi_class.Start_activity;
 
-public class CourseDetailsActivity extends AppCompatActivity {
+public class CourseDetailsActivity extends AppCompatActivity implements View.OnClickListener{
 
     private String course_code;
-    private ImageView announcement_button,sign_in_button,member_button,homework_button,file_button;
+    private ImageView announcement_button,sign_in_button,member_button,homework_button,file_button,more_button;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -45,50 +45,16 @@ public class CourseDetailsActivity extends AppCompatActivity {
         member_button = findViewById(R.id.member_button);
         homework_button = findViewById(R.id.homework_button);
         file_button = findViewById(R.id.file_button);
+        more_button = findViewById(R.id.more_button);
 
-
-
-        announcement_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CourseDetailsActivity.this, AnnouncementActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        sign_in_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CourseDetailsActivity.this, SignInActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        member_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CourseDetailsActivity.this, MemberActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        homework_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CourseDetailsActivity.this,HomeworkActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        file_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CourseDetailsActivity.this,FileActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        announcement_button.setOnClickListener(this);
+        sign_in_button.setOnClickListener(this);
+        member_button.setOnClickListener(this);
+        homework_button.setOnClickListener(this);
+        file_button.setOnClickListener(this);
+        more_button.setOnClickListener(this);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -98,5 +64,37 @@ public class CourseDetailsActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+            case R.id.announcement_button:
+                intent = new Intent(CourseDetailsActivity.this, AnnouncementActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.sign_in_button:
+                intent = new Intent(CourseDetailsActivity.this, SignInActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.member_button:
+                intent = new Intent(CourseDetailsActivity.this, MemberActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.homework_button:
+                intent = new Intent(CourseDetailsActivity.this,HomeworkActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.file_button:
+                intent = new Intent(CourseDetailsActivity.this,FileActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.more_button:
+
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/mi_class/fragment/CourseFragment.java
+++ app/src/main/java/com/example/mi_class/fragment/CourseFragment.java
@@ -2,25 +2,21 @@ package com.example.mi_class.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.mi_class.MainActivity;
 import com.example.mi_class.R;
 import com.example.mi_class.activity.CourseDetailsActivity;
 import com.example.mi_class.adapter.CourseAdapter;
-import com.example.mi_class.adapter.MessageAdapter;
 import com.example.mi_class.domain.Course;
-import com.example.mi_class.domain.Message;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +26,8 @@ public class CourseFragment extends Fragment {
     private ListView course_list_view;
     private List<Course> course_list;
     private CourseAdapter course_adapter;
+    private TextView is_null;
+    private boolean is_show = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,6 +35,7 @@ public class CourseFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_courses, container, false);
 
         course_list_view = (ListView) view.findViewById(R.id.course_list);
+        is_null = (TextView) view.findViewById(R.id.is_null);
 
         // 默认数据显示用
         Course course = new Course("C语言","课程码:wumu","2019-2020\n第二学期","99人已加入");
@@ -44,9 +43,13 @@ public class CourseFragment extends Fragment {
         course_list.add(course);
         course_list.add(course);
 
-        course_adapter = new CourseAdapter(getActivity(), R.layout.fragment_courses_list,course_list);     //初始化适配器
-        course_list_view.setAdapter(course_adapter); //设置适配器，显示查询结果
-
+        //判断用户是否有添加课程设置视图
+        if(is_show) {
+            course_adapter = new CourseAdapter(getActivity(), R.layout.fragment_courses_list, course_list);     //初始化适配器
+            course_list_view.setAdapter(course_adapter); //设置适配器，显示查询结果
+        } else{
+            is_null.setVisibility(View.VISIBLE);
+        }
         return view;
 
 
