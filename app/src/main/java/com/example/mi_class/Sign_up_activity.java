89d6be696ae7263866a5768947f89c21f1cf21d4
AmberJL <38620244+AmberJL@users.noreply.github.com>
2020-10-19 21:17:27 +0800

    注释了注册页面的部分代码

--- app/src/main/java/com/example/mi_class/Sign_up_activity.java
+++ app/src/main/java/com/example/mi_class/Sign_up_activity.java
@@ -249,25 +249,25 @@ public class Sign_up_activity extends AppCompatActivity {
 
 
 
-        RegisterPage page = new RegisterPage();
-        //如果使用我们的ui，没有申请模板编号的情况下需传null
-        page.setTempCode(null);
-        page.setRegisterCallback(new EventHandler() {
-            public void afterEvent(int event, int result, Object data) {
-                if (result == SMSSDK.RESULT_COMPLETE) {
-                    // 处理成功的结果
-                    HashMap<String,Object> phoneMap = (HashMap<String, Object>) data;
-                    // 国家代码，如“86”
-                    String country = (String) phoneMap.get("country");
-                    // 手机号码，如“13800138000”
-                    String phone = (String) phoneMap.get("phone");
-                    // TODO 利用国家代码和手机号码进行后续的操作
-                } else{
-                    // TODO 处理错误的结果
-                }
-            }
-        });
-        page.show(a);
+//        RegisterPage page = new RegisterPage();
+//        //如果使用我们的ui，没有申请模板编号的情况下需传null
+//        page.setTempCode(null);
+//        page.setRegisterCallback(new EventHandler() {
+//            public void afterEvent(int event, int result, Object data) {
+//                if (result == SMSSDK.RESULT_COMPLETE) {
+//                    // 处理成功的结果
+//                    HashMap<String,Object> phoneMap = (HashMap<String, Object>) data;
+//                    // 国家代码，如“86”
+//                    String country = (String) phoneMap.get("country");
+//                    // 手机号码，如“13800138000”
+//                    String phone = (String) phoneMap.get("phone");
+//                    // TODO 利用国家代码和手机号码进行后续的操作
+//                } else{
+//                    // TODO 处理错误的结果
+//                }
+//            }
+//        });
+//        page.show(a);
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
